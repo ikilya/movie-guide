f85dc940c00121e992d88fcff098feb3f81455e4
ikilya <kelembet95@gmail.com>
2020-02-18 23:08:08 +0200

    Movies sort is added

--- movie-guide/src/reducers/movieReducer.js
+++ movie-guide/src/reducers/movieReducer.js
@@ -8,15 +8,31 @@ const initialState = {
 const movieReducer = function(state = initialState, action) {
     switch(action.type) {
         case types.GET_MOVIES_SUCCESS:
+            const newMovies = action.movies;
+            newMovies.sort(compareMovieNames);
             return {...state, movies: action.movies};
-        case types.SET_MOVIE_SUCCESS:
+        case types.SET_MOVIE_SUCCESS: {
             const newMovies = [...state.movies, action.movie];
+            newMovies.sort(compareMovieNames);
             return {...state, movies: newMovies};
+        }
         case types.GET_MOVIE_FORMATS_SUCCESS:
             return {...state, movieFormats: action.movieFormats};
     }
 
     return state;
 };
+
+function compareMovieNames(movieA, movieB) {
+    const a = movieA.title.toLowerCase();
+    const b = movieB.title.toLowerCase();
+    if (a > b) {
+        return 1;
+    }
+    if (a < b) {
+        return -1;
+    }
+    return 0;
+}
 
 export default movieReducer;
